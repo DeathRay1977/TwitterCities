7059473c24b429c3a74e5cdc6ea1c0fd1419967f
Norm Fasey <norm@fasey.net>
2015-11-16 21:04:15 +0000

    User Schema

---
+++ spec/userSpec.js
@@ -0,0 +1,58 @@
+describe("User", function() {
+  var mongoose = require('mongoose');
+  var User = require('../src/user');
+  var testUser;
+  var testUsername = 'Jimbob2000';
+  var goodPassword = 'Password123';
+  var badPassword = 'IAmHacker';
+  var connStr = 'mongodb://localhost:27017/mongoose-bcrypt-test';
+
+  mongoose.connect(connStr, function(err) {
+    if (err) throw err;
+  });
+
+  beforeEach(function() {
+    User.remove({}, function(err) {
+      if (err) throw err;
+    });
+
+    // create a user a new user
+    var testUser = new User({
+      username: testUsername,
+      password: goodPassword
+    });
+
+    // save user to database
+    testUser.save(function(err) {
+      if (err) throw err;
+    });
+  });
+
+  it('Confirms a matching password', function() {
+    User.findOne({
+      username: testUsername
+    }, function(err, user) {
+      if (err) throw err;
+      expect(function() {
+        // test a matching password
+        user.comparePassword(goodPassword, function(err, isMatch) {
+          if (err) throw err;
+        });
+      }).not.toThrowError();
+    });
+  });
+
+  it('Does not match a bad password', function() {
+    User.findOne({
+      username: testUsername
+    }, function(err, user) {
+      if (err) throw err;
+      expect(function() {
+        // test a failing password
+        user.comparePassword(badPassword, function(err, isMatch) {
+          if (err) throw err;
+        });
+      }).not.toThrowError();
+    });
+  });
+});
